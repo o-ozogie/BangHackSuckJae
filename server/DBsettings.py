003be44683b7a76e5b0e0e4b0162b645f114e554
wjd030811 <49752899+wjd030811@users.noreply.github.com>
2019-08-16 13:49:05 +0900

    Update DBsettings.py

--- server/DBsettings.py
+++ server/DBsettings.py
@@ -1,4 +1,4 @@
 import pymysql
 
-conn=pymysql.connect(host='localhost',user='root',password='dndud030811',db='minitweet',charset='utf8')
+conn=pymysql.connect(host='localhost',user='root',password='variable',db='minitweet',charset='utf8')
 curs=conn.cursor(pymysql.cursors.DictCursor)
